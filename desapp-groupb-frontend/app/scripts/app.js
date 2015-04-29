09e25bc83784a377086d006284dc0432b2628b2e
fernandodm <dimegliofernando@gmail.com>
2015-04-29 04:03:35 -0300

    se agrego proyecto angular

---
+++ desapp-groupb-frontend/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myappApp
+ * @description
+ * # myappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/historiasClinicas', {
+        templateUrl: 'views/historiasClinicas.html',
+        controller: 'HistoriasClinicasCtrl'
+      })
+      .when('/crearPaciente', {
+        templateUrl: 'views/crearPaciente.html',
+        controller: 'CrearPacienteCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ desapp-groupb-frontend/app/scripts/controllers/crearPaciente.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myappApp.controller:HistoriasClinicasCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myappApp
+ */
+angular.module('myappApp')
+  .controller('CrearPacienteCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
