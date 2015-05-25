1679e59b6ad378e65b1d879c11304ede3fdc7ddd
fernandodm <dimegliofernando@gmail.com>
2015-05-25 19:02:50 -0300

    se agrego crear paciente y listarlos (no se envia bien los datos al rest)

--- desapp-groupb-frontend/app/scripts/controllers/crearPaciente.js
+++ desapp-groupb-frontend/app/scripts/controllers/crearPaciente.js
@@ -8,10 +8,16 @@
  * Controller of the myappApp
  */
 angular.module('myappApp')
-  .controller('CrearPacienteCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CrearPacienteCtrl', function ($http,$scope) {
+    //$scope.objectPaciente = { 'firstname':'', 'lastname':'', 'dni':''};
+    $scope.crearPaciente = function() {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http.post('http://localhost:8080/desapp-groupb-backend/rest/patients/create/', {firstname: $scope.firstname, lastname: $scope.lastname, dni: $scope.dni})
+        .success(function() {
+                alert('"Paciente "' + $scope.firstname +'", creado correctamente!!');
+                
+        }).error(function(data,status) {
+            alert('No se pudo crear el paciente, error (' + status + ')');
+        });
+    };
   });
--- desapp-groupb-frontend/app/scripts/controllers/historiasClinicas.js
+++ desapp-groupb-frontend/app/scripts/controllers/historiasClinicas.js
@@ -8,10 +8,11 @@
  * Controller of the myappApp
  */
 angular.module('myappApp')
-  .controller('HistoriasClinicasCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HistoriasClinicasCtrl', function ($http,$scope) {
+  	
+    $http.get('http://localhost:8080/desapp-groupb-backend/rest/patients/list/').success(function (data) {
+       //datos lo tenemos disponible en la vista gracias a $scope
+       $scope.pacientes = data;
+    });
   });
+
