b0b02373303c8dd781d7e5b79956b9f61975d1a8
fernandodm <dimegliofernando@gmail.com>
2015-05-25 21:35:37 -0300

    agregar paciente funciona ya bien

--- desapp-groupb-frontend/app/scripts/controllers/crearPaciente.js
+++ desapp-groupb-frontend/app/scripts/controllers/crearPaciente.js
@@ -9,15 +9,30 @@
  */
 angular.module('myappApp')
   .controller('CrearPacienteCtrl', function ($http,$scope) {
-    //$scope.objectPaciente = { 'firstname':'', 'lastname':'', 'dni':''};
     $scope.crearPaciente = function() {
+       /**
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         $http.post('http://localhost:8080/desapp-groupb-backend/rest/patients/create/', {firstname: $scope.firstname, lastname: $scope.lastname, dni: $scope.dni})
-        .success(function() {
+        .success(function(data) {
                 alert('"Paciente "' + $scope.firstname +'", creado correctamente!!');
-                
         }).error(function(data,status) {
             alert('No se pudo crear el paciente, error (' + status + ')');
-        });
+        });**/
+
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/desapp-groupb-backend/rest/patients/create/',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                    for(var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+            },
+            data: {firstname: $scope.firstname, lastname: $scope.lastname, dni: $scope.dni}
+        }).success(function (data) {
+                alert('"Paciente "' + $scope.firstname +'", creado correctamente!!');
+            }).error(function(data,status) {
+                alert('No se pudo crear el paciente, error (' + status + ')');});
     };
   });
