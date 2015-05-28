e914e5fb0ac26175ccede068a04a1f3b3a92caf6
fernandodm <dimegliofernando@gmail.com>
2015-05-27 23:44:40 -0300

    se agrego agregar alergias y eliminarlas al paciente

--- desapp-groupb-frontend/app/scripts/controllers/verHistoria.js
+++ desapp-groupb-frontend/app/scripts/controllers/verHistoria.js
@@ -14,6 +14,32 @@ angular.module('myappApp')
 	$http.get('http://localhost:8080/desapp-groupb-backend/rest/patients/' + $routeParams.id).success(function (data) {
        //datos lo tenemos disponible en la vista gracias a $scope
        $scope.paciente = data;
+       $scope.alergias = $scope.paciente.medicalHistory.allergies;
 
     });
+
+    $scope.agregarAlergia = function() {
+		$scope.alergias.push($scope.alergia);
+
+		$http.put('http://localhost:8080/desapp-groupb-backend/rest/patients/update/' + $routeParams.id + '/' + $scope.alergia)
+			.success(function () {
+       
+  
+
+    	});
+
+	};
+
+	$scope.eliminarAlergia = function(alergia) {
+		var index = $scope.alergias.indexOf(alergia);
+		$scope.alergias.splice(index,1);
+		$http.put('http://localhost:8080/desapp-groupb-backend/rest/patients/delete/' + $routeParams.id + '/' + alergia)
+			.success(function () {
+       
+  
+    	});
+
+
+	};
+
   });
