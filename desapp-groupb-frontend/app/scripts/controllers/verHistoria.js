0a2fc1c769d5f2399c25ed27c484646ca17570cd
fernandodm <dimegliofernando@gmail.com>
2015-07-16 01:38:23 -0300

    se agrego tablas

--- desapp-groupb-frontend/app/scripts/controllers/verHistoria.js
+++ desapp-groupb-frontend/app/scripts/controllers/verHistoria.js
@@ -8,7 +8,73 @@
  * Controller of the myappApp
  */
 angular.module('myappApp')
-  .controller('VerHistoriaCtrl', function ($scope,$http,$routeParams) {
+  .controller('VerHistoriaCtrl', VerHistoriaCtrl);
+
+
+  function VerHistoriaCtrl($http, $routeParams, $resource, DTOptionsBuilder, DTColumnDefBuilder) {
+    
+    var vm = this;
+     
+    $http.get('http://localhost:8080/desapp-groupb-backend/rest/patients/list/').success(function (data) {
+       vm.pacientes = data;
+    });
+
+    vm.dtOptions = DTOptionsBuilder.newOptions()
+                            .withOption('info',false)
+                            .withDisplayLength(10)
+                            .withBootstrap();
+    vm.dtColumnDefs = [
+        DTColumnDefBuilder.newColumnDef(0),
+        DTColumnDefBuilder.newColumnDef(1).notSortable()
+    ];
+
+    $http.get('http://localhost:8080/desapp-groupb-backend/rest/patients/' + $routeParams.id).success(function (paciente) {
+       vm.alergias = paciente.medicalHistory.allergies;
+       $http.get('http://localhost:8080/desapp-groupb-backend/rest/patients/diagnoses/' + $routeParams.id).success(function (diagnosticos) {
+          vm.diagnosticos = diagnosticos;
+        });
+    });
+
+    vm.agregarAlergia = function() {
+
+      if(vm.alergias.indexOf(vm.alergia) == -1){
+        vm.alergias.push(vm.alergia);
+      
+      $http.put('http://localhost:8080/desapp-groupb-backend/rest/patients/update/' + $routeParams.id + '/' + vm.alergia)
+        .success(function () {
+       
+  
+        });
+        vm.alergia = "";
+    }
+
+  };
+
+  vm.eliminarAlergia = function(alergia) {
+    var index = vm.alergias.indexOf(alergia);
+    vm.alergias.splice(index,1);
+    $http.put('http://localhost:8080/desapp-groupb-backend/rest/patients/delete/' + $routeParams.id + '/' + alergia)
+      .success(function () {
+       
+  
+      });
+
+  };
+
+  vm.agregarDiagnostico = function() {
+    
+        location = '#/agregarDiagnostico/' + $routeParams.id;   
+    
+  };
+
+  vm.verDiagnostico = function(diagnostico) {
+    
+        location = '#/verDiagnostico/' + $routeParams.id + '/' + diagnostico.id;   
+    
+  };
+}
+
+  /*function ($scope,$http,$routeParams) {
   	    
 	$http.get('http://localhost:8080/desapp-groupb-backend/rest/patients/' + $routeParams.id).success(function (paciente) {
        //datos lo tenemos disponible en la vista gracias a $scope
@@ -56,4 +122,4 @@ angular.module('myappApp')
     
   };
 
-});
+});*/
